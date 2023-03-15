bbe2fa258f78e680ce4626cf6f627029f25aaedf
BIKRAM-SAHA <bikramlearn15@gmail.com>
2023-03-16 02:21:23 +0530

    add folder structure for navbar and footer

---
+++ components/commons/footer/Footer.tsx
@@ -0,0 +1,6 @@
+import { FooterProps } from "./Footer.types";
+import "./styles/Footer.module.scss";
+
+export default function Footer({}: FooterProps) {
+  return <div>Footer here</div>;
+}
---
+++ components/commons/navbar/Navbar.tsx
@@ -0,0 +1,6 @@
+import { NavbarProps } from "./Navbar.types";
+import "./styles/Navbar.module.scss";
+
+export default function Navbar({}: NavbarProps) {
+  return <div>Navbar here</div>;
+}
