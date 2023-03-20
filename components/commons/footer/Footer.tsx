f8c17df3e711b1221d2be16d39182b83197319da
Faiz Bachoo Shah <faizbshah2001@gmail.com>
2023-03-20 17:57:10 +0530

    Deleted folder

--- components/commons/footer/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from "react";
-import { FooterProps as Props } from "./Footer.types";
-import styles from "./styles/Footer.module.scss";
-
-const Footer: FC<Props> = ({}) => {
-  return <div>Footer here</div>;
-};
-
-export default Footer;
