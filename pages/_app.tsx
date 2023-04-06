df5ef9ac2e3ea8152648c581df7498223e1025c5
Gargi Roy <itsgargiroy@gmail.com>
2023-04-06 14:36:22 +0530

    Created Footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.scss'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.scss";
+import type { AppProps } from "next/app";
+import Footer from "@/components/commons/Footer/Footer";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
